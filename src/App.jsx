9313fbb02c3f61e1622369aee722c1b1f31c52f1
Rahidul Islam <33887458+rahidulislam@users.noreply.github.com>
2023-08-21 13:48:51 +0600

    useNavigate is added

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import { BrowserRouter,Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Blogs from './pages/Blogs'
 import Contact from './pages/Contact'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
+    <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/blogs" element={<Blogs/>}/>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const navigate = useNavigate();
+    return (
+        <div>
+            <h3>Contact</h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
+                deserunt id ullam dolores sit dolore unde iure laboriosam
+                doloremque numquam consequuntur explicabo nam ipsa, quibusdam
+                quidem, placeat assumenda fugit aliquam.
+            </p>
+            <button onClick={() => navigate("/")}>Go To Home</button>
+        </div>
+    );
+};
 
-export default Contact
+export default Contact;
