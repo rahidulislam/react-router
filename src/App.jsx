d995649a739eb38c473f789a186aecbe0c8a9b5e
Rahidul Islam <33887458+rahidulislam@users.noreply.github.com>
2023-08-21 09:42:52 +0600

    react router is added

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter,Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Blogs from './pages/Blogs'
+import Contact from './pages/Contact'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/blogs" element={<Blogs/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
